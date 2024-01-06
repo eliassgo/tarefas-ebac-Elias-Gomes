f808dc6c45e9e15524b95b595cd334ffbf7ad2e2
Elias Gomes <eliasgo221@gmail.com>
2024-01-06 11:37:23 -0300

    refact: modifying mediaNotas function

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         mediaNotas();
     }
     private static void mediaNotas() {
-        int nota1 = 10;
+        int nota1 = 9;
         int nota2 = 7;
         int nota3 = 9;
         int nota4 = 4;
 
-        int media = (nota1 + nota2 + nota3 + nota4) / 4;
+        double media = (nota1 + nota2 + nota3 + nota4) / 4.0;
         
         System.out.println(media);
     }
