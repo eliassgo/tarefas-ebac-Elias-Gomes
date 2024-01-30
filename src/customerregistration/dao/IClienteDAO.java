38532573a93391287008ca8924c38b2c8173d722
Elias Gomes <eliasgo221@gmail.com>
2024-01-30 17:56:06 -0300

    feat: add main frame

---
+++ src/customerregistration/dao/IClienteDAO.java
@@ -0,0 +1,23 @@
+package customerregistration.dao;
+
+import customerregistration.domain.Cliente;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author elias
+ */
+public interface IClienteDAO {
+    
+    public Boolean cadastrar(Cliente cliente);
+    
+    public void excluir(Long cpf);
+    
+    public void alterar(Cliente cliente);
+    
+    public Cliente consultar(Long cpf);
+}
